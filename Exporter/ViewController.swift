a988089d9fc359ca2d7595d2cb49832853251005
Condy <yangkj310@gmail.com>
2022-12-27 17:19:33 +0800

    Update test case

--- Exporter/ViewController.swift
+++ Exporter/ViewController.swift
@@ -14,9 +14,42 @@ import Harbeth
 
 class ViewController: UIViewController {
     
+    let videoURL: URL = URL(string: "https://mp4.vjshi.com/2017-11-21/7c2b143eeb27d9f2bf98c4ab03360cfe.mp4")!
+    
+    // Creating temp path to save the converted video
+    let outputURL: URL = {
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0] as URL
+        let outputURL = documentsDirectory.appendingPathComponent("condy_exporter_video.mp4")
+        
+        // Check if the file already exists then remove the previous file
+        if FileManager.default.fileExists(atPath: outputURL.path) {
+            do {
+                try FileManager.default.removeItem(at: outputURL)
+            } catch {
+                //completionHandler(nil, error)
+            }
+        }
+        return outputURL
+    }()
+    
+    lazy var player: AVPlayer = {
+        let playerItem = AVPlayerItem(url: videoURL)
+        let player = AVPlayer(playerItem: playerItem)
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.videoGravity = .resizeAspect
+        playerView.layoutIfNeeded()
+        playerLayer.frame = playerView.bounds
+        playerView.layer.addSublayer(playerLayer)
+        return player
+    }()
+    
     lazy var playerView: UIView = {
         let view = UIView.init()
         view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor.systemPink.withAlphaComponent(0.4)
+        let singleTapGesture = UITapGestureRecognizer(target: self, action: #selector(handleSingleTap))
+        singleTapGesture.numberOfTapsRequired = 1
+        view.addGestureRecognizer(singleTapGesture)
         return view
     }()
     
@@ -26,9 +59,9 @@ class ViewController: UIViewController {
         button.imageView?.contentMode = .scaleAspectFit
         button.translatesAutoresizingMaskIntoConstraints = false
         button.backgroundColor = UIColor.green.withAlphaComponent(0.5)
-        button.layer.cornerRadius = 20
+        button.layer.cornerRadius = 15
         button.layer.masksToBounds = true
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 25)
         button.addTarget(self, action: #selector(videoAction), for: UIControl.Event.touchUpInside)
         return button
     }()
@@ -39,12 +72,17 @@ class ViewController: UIViewController {
         button.imageView?.contentMode = .scaleAspectFit
         button.translatesAutoresizingMaskIntoConstraints = false
         button.backgroundColor = UIColor.green.withAlphaComponent(0.5)
-        button.layer.cornerRadius = 20
+        button.layer.cornerRadius = 15
         button.layer.masksToBounds = true
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 25)
         button.addTarget(self, action: #selector(photoAction), for: UIControl.Event.touchUpInside)
         return button
     }()
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        player.pause()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -60,65 +98,28 @@ class ViewController: UIViewController {
         view.addSubview(photoButton)
         NSLayoutConstraint.activate([
             playerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            playerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 20),
-            playerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -20),
-            playerView.heightAnchor.constraint(equalTo: playerView.widthAnchor, multiplier: 16/9),
-            videoButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -80),
+            playerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            playerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            playerView.heightAnchor.constraint(equalTo: playerView.widthAnchor, multiplier: 3/4),
+            videoButton.topAnchor.constraint(equalTo: playerView.bottomAnchor, constant: 30),
             videoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            videoButton.widthAnchor.constraint(equalToConstant: 200),
-            videoButton.heightAnchor.constraint(equalToConstant: 80),
-            photoButton.topAnchor.constraint(equalTo: videoButton.bottomAnchor, constant: 50),
+            videoButton.widthAnchor.constraint(equalToConstant: 180),
+            videoButton.heightAnchor.constraint(equalToConstant: 70),
+            photoButton.topAnchor.constraint(equalTo: videoButton.bottomAnchor, constant: 30),
             photoButton.centerXAnchor.constraint(equalTo: videoButton.centerXAnchor),
-            photoButton.widthAnchor.constraint(equalToConstant: 200),
-            photoButton.heightAnchor.constraint(equalToConstant: 80),
+            photoButton.widthAnchor.constraint(equalToConstant: 180),
+            photoButton.heightAnchor.constraint(equalToConstant: 70),
         ])
     }
     
     func setupPlayer() {
-        let url = URL(string: "https://mp4.vjshi.com/2017-11-21/7c2b143eeb27d9f2bf98c4ab03360cfe.mp4")!
-        let playerItem = AVPlayerItem(url: url)
-        let player = AVPlayer(playerItem: playerItem)
-        let playerLayer = AVPlayerLayer(player: player)
-        // 显示比例，默认resizeAspect，会在承载范围内缩放视频大小保持原始宽高比
-        // resizeAspectFill会保持视频的宽高比，并使其通过缩放填满层的范围区域，通常会导致图像被部分裁剪
-        // resize会将视频内容拉伸来匹配承载层的范围，这种情况最不常用，因为他通常会导致图片扭曲而导致的funhouse effect效应
-        playerLayer.videoGravity = .resize
-        //playerLayer.frame = CGRect(x: 0, y: 100, width: SCREEN_WIDTH, height: 300)
-        playerView.layer.addSublayer(playerLayer)
         player.play()
     }
 }
 
 extension ViewController {
-    
-    @objc func photoAction() {
-        let cameraPickerViewController = UIImagePickerController()
-        cameraPickerViewController.delegate = self
-        cameraPickerViewController.mediaTypes = [kUTTypeMovie as String]
-        cameraPickerViewController.videoQuality = .typeMedium
-        self.present(cameraPickerViewController, animated: true)
-    }
-    
-    @objc func videoAction() {
-        let url = URL(string: "https://mp4.vjshi.com/2017-11-21/7c2b143eeb27d9f2bf98c4ab03360cfe.mp4")!
-        self.export(at: url)
-    }
-    
     func export(at url: URL) {
-        let exporter = Exporter.init(videoURL: url)
-        
-        // Creating temp path to save the converted video
-        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0] as URL
-        let outputURL = documentsDirectory.appendingPathComponent("condy_exporter_video.mp4")
-        
-        // Check if the file already exists then remove the previous file
-        if FileManager.default.fileExists(atPath: outputURL.path) {
-            do {
-                try FileManager.default.removeItem(at: outputURL)
-            } catch {
-                //completionHandler(nil, error)
-            }
-        }
+        self.view.makeToast("Exporting..", duration: 600, position: .center)
         
         let filters: [C7FilterProtocol] = [
             C7Flip(horizontal: true, vertical: false),
@@ -127,8 +128,7 @@ extension ViewController {
             C7WhiteBalance(temperature: 5000),
         ]
         
-        self.view.makeToast("Exporting..", duration: 600, position: .center)
-        
+        let exporter = Exporter.init(videoURL: url)
         exporter.export(outputURL: outputURL, filtering: { (buffer) in
             let dest = BoxxIO(element: buffer, filters: filters)
             return (try? dest.output()) ?? buffer
@@ -142,6 +142,29 @@ extension ViewController {
                 vc.player?.play()
             }
         })
+    }
+}
+
+extension ViewController {
+    
+    @objc func handleSingleTap() {
+        if player.timeControlStatus == .playing {
+            player.pause()
+        } else {
+            player.play()
+        }
+    }
+    
+    @objc func photoAction() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.mediaTypes = [kUTTypeMovie as String]
+        picker.videoQuality = .typeMedium
+        self.present(picker, animated: true)
+    }
+    
+    @objc func videoAction() {
+        self.export(at: videoURL)
     }
 }
 
--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # Exporter
 Video Filter Exporter.
 
-这是一款注入滤镜的视频框架，支持网络URL和本地URL，同时也支持相册视频。
-
-
+It's a filter infused video tool that supports network and local urls, as well as album videos.
